4ed080210d5810f26800b20a8c974ab6917f7a2a
Elmotrix <kim.hoftvedt@gmail.com>
2023-03-08 19:45:36 +0100

    Add project files.

---
+++ TraderHashMod/BepInEx.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TraderHashMod
+{
+    #region BepInEx
+    [BepInEx.BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class TraderHashBepInEx : BepInEx.BaseUnityPlugin
+    {
+        public const string pluginGuid = "net.elmo.stationeers.TraderHash";
+        public const string pluginName = "TraderHash";
+        public const string pluginVersion = "1.0";
+        public static void Log(string line)
+        {
+            Debug.Log("[" + pluginName + "]: " + line);
+        }
+        void Awake()
+        {
+            try
+            {
+                var harmony = new Harmony(pluginGuid);
+                harmony.PatchAll();
+                Log("Patch succeeded");
+            }
+            catch (Exception e)
+            {
+                Log("Patch Failed");
+                Log(e.ToString());
+            }
+        }
+    }
+    #endregion
+}
---
+++ TraderHashMod/TraderHash.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts;
+using Assets.Scripts.Objects.Electrical;
+using Assets.Scripts.Objects.Motherboards;
+using HarmonyLib;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+namespace TraderHashMod
+{
+    [HarmonyPatch(typeof(SatelliteDish), nameof(SatelliteDish.GetLogicValue))]
+    public class TraderHash
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(LogicType logicType, TraderContact ____strongestContact, ref double __result)
+        {
+            if (logicType == LogicType.SignalID && ____strongestContact != null)
+            {
+                string str = ____strongestContact.DisplayName;
+                int value = Hash128.Parse(str).GetHashCode();
+                __result = value;
+                return false;
+            }
+            return true;
+        }
+    }
+}
